97e87923dd02e5caa0d59efd05dbacf8e8471f5e
Mathieu-Andre Chiasson <mchiasson@gmail.com>
2020-07-20 22:28:58 -0400

    wip

--- src/Texture.cpp
+++ src/Texture.cpp
@@ -3,69 +3,51 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
 
+#include <QDebug>
+
 Texture::Bitmap::~Bitmap()
 {
-    if (data)
+    if (decodedData)
     {
-        stbi_image_free(data);
-        data = nullptr;
+        stbi_image_free(decodedData);
+        decodedData = nullptr;
     }
 }
 
 Texture::Texture(Node *parent) : Node(parent)
 {
+    connect(this, SIGNAL(sourceChanged), this, SLOT(loadImage), Qt::QueuedConnection);
+    connect(this, SIGNAL(targetChanged), this, SLOT(loadImage), Qt::QueuedConnection);
+    connect(this, SIGNAL(formatChanged), this, SLOT(loadImage), Qt::QueuedConnection);
     connect(&m_dataFutureWatcher, &QFutureWatcher<QByteArray>::finished, this, &Texture::onImageDecodingComplete);
 }
 
-void Texture::setSource(QString newSource)
+void Texture::setSource(const QString &newSource)
 {
     if (m_source != newSource)
     {
         m_source = newSource;
         emit sourceChanged(newSource);
-
-        setStatus(Loading);
-
-        // Load the scene from a thread in Qt's thread pool.
-        m_dataFutureWatcher.setFuture(QtConcurrent::run([](QString path)->QSharedPointer<Bitmap> {
-            QSharedPointer<Bitmap> bitmap(new Bitmap);
-
-            QFile content;
-            if (path.startsWith("qrc:", Qt::CaseInsensitive))
-            {
-                content.setFileName(path.mid(3)); // Keep the ':'. This is not a mistake.
-            }
-            else if (path.startsWith("file:", Qt::CaseInsensitive))
-            {
-                content.setFileName(QUrl(path).toLocalFile());
-            }
-            else
-            {
-                content.setFileName(path);
-            }
-
-            if (content.open(QFile::ReadOnly))
-            {
-                QByteArray encodedData = content.readAll();
-                bitmap->data = stbi_load_from_memory((const uint8_t *)encodedData.constData(), encodedData.size(), &bitmap->width, &bitmap->height, &bitmap->channelCount, STBI_rgb_alpha);
-
-                if (!bitmap->data)
-                {
-                    qCritical() << "Could not decode" << path << ":" << stbi_failure_reason();
-                }
-
-                content.close();
-            }
-            else
-            {
-                qCritical() << path << "not found!";
-            }
-
-            return bitmap;
-        }, newSource));
-
     }
 
+}
+
+void Texture::setTarget(Target newTarget)
+{
+    if (m_target != newTarget)
+    {
+        m_target = newTarget;
+        emit targetChanged(newTarget);
+    }
+}
+
+void Texture::setFormat(Format newFormat)
+{
+    if (m_format != newFormat)
+    {
+        m_format = newFormat;
+        emit formatChanged(newFormat);
+    }
 }
 
 void Texture::setStatus(Status newStatus)
@@ -136,16 +118,61 @@ void Texture::setSampleMode(int sm)
     }
 }
 
+void Texture::loadImage() {
+    if (status() != StatusLoading)
+    {
+        setStatus(StatusLoading);
+        // Load the scene from a thread in Qt's thread pool.
+        m_dataFutureWatcher.setFuture(QtConcurrent::run([](QString path, Target target, Format format)->QSharedPointer<Bitmap> {
+            QSharedPointer<Bitmap> bitmap(new Bitmap);
+            bitmap->target = target;
+
+            QFile content;
+            if (path.startsWith("qrc:", Qt::CaseInsensitive))
+            {
+                content.setFileName(path.mid(3)); // Keep the ':'. This is not a mistake.
+            }
+            else if (path.startsWith("file:", Qt::CaseInsensitive))
+            {
+                content.setFileName(QUrl(path).toLocalFile());
+            }
+            else
+            {
+                content.setFileName(path);
+            }
+
+            if (content.open(QFile::ReadOnly))
+            {
+                QByteArray encodedData = content.readAll();
+                bitmap->decodedData = stbi_load_from_memory((const uint8_t *)encodedData.constData(), encodedData.size(), &bitmap->width, &bitmap->height, &bitmap->channelCount, format);
+
+                if (!bitmap->decodedData)
+                {
+                    qCritical() << "Could not decode" << path << ":" << stbi_failure_reason();
+                }
+
+                content.close();
+            }
+            else
+            {
+                qCritical() << path << "not found!";
+            }
+
+            return bitmap;
+        }, m_source, (Target)m_target, m_format));
+    }
+}
+
 void Texture::onImageDecodingComplete()
 {
     QSharedPointer<Bitmap> bitmap = m_dataFutureWatcher.result();
-    if (bitmap->data)
+    if (bitmap->decodedData)
     {
-        setStatus(Processing);
+        setStatus(StatusProcessing);
     }
     else
     {
-        setStatus(Error);
+        setStatus(StatusError);
     }
 }
 
@@ -153,138 +180,61 @@ void Texture::onUpdate()
 {
     switch(status())
     {
-    case Processing:
+    case StatusProcessing:
     {
         QSharedPointer<Bitmap> bitmap = m_dataFutureWatcher.result();
 
-        //TODO
-        int format = Texture_RGBA;
-        int sampleMode = Sample_Anisotropic | Sample_Clamp;
+        GLenum glformat, gltype = GL_UNSIGNED_BYTE;
+        switch(bitmap->channelCount)
+        {
+        case 1:
+            glformat = GL_RED;
+            break;
+        case 2:
+            glformat = GL_RG;
+            break;
+        case 3:
+            glformat = GL_RGB;
+            break;
+        case 4:
+            glformat = GL_RGBA;
+            break;
+        default:
+            qCritical() << "Unknown texture format:" << bitmap->channelCount;
+            setStatus(StatusError);
+            return;
+        }
 
-        if (handle() == 0)
+        GLenum glTarget;
+        switch (bitmap->target)
+        {
+        case TargetTexture2D:
+            glTarget = GL_TEXTURE_2D;
+            device()->glEnable(GL_TEXTURE_2D);
+            break;
+        case TargetTextureCubeMap:
+            glTarget = GL_TEXTURE_CUBE_MAP;
+            device()->glEnable(GL_TEXTURE_CUBE_MAP);
+            break;
+        default:
+            qCritical() << "Unknown texture target:" << bitmap->target;
+            setStatus(StatusError);
+            return;
+        }
+
+        if (!handle())
         {
             device()->glGenTextures(1, &m_handle);
         }
 
-        bool isDepth = ((format & Texture_DepthMask) != 0);
-        bool isSRGB  = ((format & Texture_SRGB) != 0);
-        Q_ASSERT(!(isSRGB && isDepth) && "Depth formats and sRGB are incompatible texture flags.");
+        device()->glBindTexture(glTarget, handle());
+        device()->glTexImage2D(glTarget, 0, glformat, bitmap->width, bitmap->height, 0, glformat, gltype, bitmap->decodedData);
+        device()->glGenerateMipmap(glTarget);
 
-        bool isFloatFormat = (((format & Texture_TypeMask) == Texture_RGBA16f) ||
-                              ((format & Texture_TypeMask) == Texture_R11G11B10f));
+        setSampleMode(Sample_Anisotropic | Sample_Clamp);
 
-        bool isCompressed = false;
-
-        // if this is a color texture that will be sent over to or received from OVRService, then force sRGB on it
-        // we will keep rendering into it as if it's a linear-format texture
-        if (!isDepth && !isFloatFormat &&
-            ((format & Texture_SwapTextureSet) || (format & Texture_SwapTextureSetStatic) || (format & Texture_Mirror)))
-        {
-            isSRGB = true;
-        }
-
-        GLenum glformat = GL_RGBA, gltype = GL_UNSIGNED_BYTE;
-        GLenum internalFormat = isSRGB ? GL_SRGB8_ALPHA8 : glformat;
-        switch(format & Texture_TypeMask)
-        {
-        case Texture_B5G6R5:
-            glformat = GL_RGB565;
-            break;
-        case Texture_BGR5A1:
-            glformat = GL_RGB5_A1;
-            break;
-        case Texture_BGRA4:
-            glformat = GL_RGBA4;
-            break;
-        case Texture_RGBA8:
-            glformat = GL_RGBA;
-            break;
-        case Texture_BGRA8:
-            glformat = GL_BGRA;
-            break;
-        case Texture_BGRX:
-            glformat = GL_BGR;
-            break;
-        case Texture_RGBA16f:
-            glformat = GL_RGBA16F;
-            // float formats use special higher-precision internal formats and don't do sRGB
-            internalFormat = glformat;
-            break;
-        case Texture_R11G11B10f:
-            glformat = GL_R11F_G11F_B10F;
-            // float formats use special higher-precision internal formats and don't do sRGB
-            internalFormat = glformat;
-            break;
-        case Texture_R:
-            glformat = GL_RED;
-            break;
-        case Texture_A:
-            glformat = GL_ALPHA;
-            break;
-        case Texture_Depth32f:
-            glformat = GL_DEPTH_COMPONENT;
-            internalFormat = GL_ARB_depth_buffer_float ? GL_DEPTH_COMPONENT32F : GL_DEPTH_COMPONENT;
-            gltype = GL_FLOAT;
-            break;
-        case Texture_Depth16:
-            glformat = GL_DEPTH_COMPONENT;
-            internalFormat = GL_DEPTH_COMPONENT16;
-            gltype = GL_UNSIGNED_INT;
-            break;
-        case Texture_Depth24Stencil8:
-            glformat = GL_DEPTH_STENCIL;
-            internalFormat = GL_DEPTH24_STENCIL8;
-            gltype = GL_UNSIGNED_INT_24_8;
-            break;
-        case Texture_Depth32fStencil8:
-            glformat = GL_DEPTH_STENCIL;
-            internalFormat = GL_DEPTH32F_STENCIL8;
-            gltype = GL_FLOAT_32_UNSIGNED_INT_24_8_REV;
-            break;
-        case Texture_BC1:
-            glformat = isSRGB ? GL_COMPRESSED_SRGB_ALPHA_S3TC_DXT1_EXT : GL_COMPRESSED_RGBA_S3TC_DXT1_EXT;
-            isCompressed = true;
-            break;
-        case Texture_BC2:
-            glformat = isSRGB ? GL_COMPRESSED_SRGB_ALPHA_S3TC_DXT3_EXT : GL_COMPRESSED_RGBA_S3TC_DXT3_EXT;
-            isCompressed = true;
-            break;
-        case Texture_BC3:
-            glformat = isSRGB ? GL_COMPRESSED_SRGB_ALPHA_S3TC_DXT5_EXT : GL_COMPRESSED_RGBA_S3TC_DXT5_EXT;
-            isCompressed = true;
-            break;
-        case Texture_BC6U:
-            glformat = GL_COMPRESSED_RGB_BPTC_UNSIGNED_FLOAT_ARB;
-            isCompressed = true;
-            break;
-        case Texture_BC6S:
-            glformat = GL_COMPRESSED_RGB_BPTC_SIGNED_FLOAT_ARB;
-            isCompressed = true;
-            break;
-        case Texture_BC7:
-            glformat = isSRGB ? GL_COMPRESSED_SRGB_ALPHA_BPTC_UNORM_ARB : GL_COMPRESSED_RGBA_BPTC_UNORM_ARB;
-            isCompressed = true;
-            break;
-        default:
-            qFatal("Unknown texture format");
-        }
-
-        m_target = GL_TEXTURE_2D;
-
-        if (format & Texture_Cubemap)
-        {
-            m_target = GL_TEXTURE_CUBE_MAP;
-            device()->glEnable(GL_TEXTURE_CUBE_MAP);
-        }
-
-        device()->glBindTexture(target(), handle());
-        device()->glTexImage2D(target(), 0, internalFormat, bitmap->width, bitmap->height, 0, glformat, gltype, bitmap->data);
-        device()->glGenerateMipmap(target());
-
-        setSampleMode(sampleMode);
-
-        stbi_image_free(bitmap->data);
-        bitmap->data = nullptr;
+        stbi_image_free(bitmap->decodedData);
+        bitmap->decodedData = nullptr;
         break;
     }
     default:
--- src/Texture.h
+++ src/Texture.h
@@ -9,35 +9,61 @@ class Texture : public Node
 {
     Q_OBJECT
     Q_PROPERTY(QString source READ source WRITE setSource NOTIFY sourceChanged)
+    Q_PROPERTY(Target target READ target WRITE setTarget NOTIFY targetChanged)
+    Q_PROPERTY(Format format READ format WRITE setFormat NOTIFY formatChanged)
     Q_PROPERTY(Status status READ status NOTIFY statusChanged)
 
 public:
+
+    enum Target
+    {
+        TargetTexture2D      = 0,
+        TargetTextureCubeMap = 1
+    };
+    Q_ENUMS(Target)
+
+    enum Format
+    {
+        FormatDefault   = 0,
+        FormatGrey      = 1,
+        FormatGreyAlpha = 2,
+        FormatRGB       = 3,
+        FormatRGBA      = 4
+    };
+    Q_ENUMS(Format)
+
     enum Status
     {
-        None = 0,
-        Loading,
-        Processing,
-        Ready,
-        Error
+        StatusNone = 0,
+        StatusLoading,
+        StatusProcessing,
+        StatusReady,
+        StatusError
     };
     Q_ENUMS(Status)
 
     struct Bitmap
     {
         ~Bitmap();
+        GLenum  target = TargetTexture2D;
         int width = 0, height = 0, channelCount = 0;
-        unsigned char *data = nullptr;
+        unsigned char *decodedData = nullptr;
     };
 
     explicit Texture(Node *parent = nullptr);
 
     QString source() const { return m_source; }
-    void setSource(QString newSource);
+    void setSource(const QString &newSource);
+
+    Target target() const { return m_target; }
+    void setTarget(Target newFormat);
+
+    Format format() const { return m_format; }
+    void setFormat(Format newFormat);
 
     Status status() const { return m_status; }
     void setStatus(Status newStatus);
 
-    GLenum target() const { return m_target; }
     GLuint handle() const { return m_handle; }
 
     void bind(int slot) const;
@@ -46,7 +72,13 @@ public:
 signals:
 
     void sourceChanged(const QString &newSource);
+    void targetChanged(Target newTarget);
+    void formatChanged(Format newFormat);
     void statusChanged(Status newStatus);
+
+protected slots:
+
+    void loadImage();
 
 protected:
 
@@ -56,8 +88,9 @@ protected:
 
 protected:
     QString m_source;
-    Status m_status = None;
-    GLenum  m_target = 0;
+    Format m_format = FormatDefault;
+    Status m_status = StatusNone;
+    Target  m_target = TargetTexture2D;
     GLuint  m_handle = 0;
     QFutureWatcher<QSharedPointer<Bitmap>> m_dataFutureWatcher;
 };
